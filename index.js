ef5531e8ac01389ad3f57ada7c8916a039fbd40c
Hassan0Anwer <153855531+Hassan0Anwer@users.noreply.github.com>
2024-03-10 20:57:14 +0500

    chapter 2
    chapters no 2

---
+++ index.js
@@ -0,0 +1,16 @@
+var userName = "Hassan";
+var surname = "Anwer";
+var userAge = "24"
+var message ="Hello Js World";
+var email = "muhammadhassananwer@gmail.com";
+var book = "A smarter way to learn JavaScript!";
+var start = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(start);
+alert("User Name:" +" "+ userName);
+alert("surname:" +" "+ surname);
+alert("User Age:" +" "+ userAge);
+alert("User Email:" +" "+ email);
+alert(message);
+alert(book);
+alert(start);
